79aa3756b337366f2620b445dbe0e342f6ca2ccf
Siddhant Jain <92942861+sid6i7@users.noreply.github.com>
2021-12-01 12:28:22 +0000

    defined a function to scrape all the videos

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import pandas as pd
 
 YOUTUBE_TRENDING_URL = "https://www.youtube.com/feed/trending"
 
@@ -25,6 +26,53 @@ def get_videos(driver):
   return videos
 
 
+def get_details(videos):
+  titles = []
+  urls = []
+  thumbnail_urls = []
+  channel_names = []
+  views = []
+  uploads = []
+  descriptions = []
+  for video in videos:
+    title_tag = video.find_element(By.ID, 'video-title')
+    url = title_tag.get_attribute('href')
+    urls.append(url)
+    title = title_tag.text
+    titles.append(title)
+
+    thumbnail_tag = video.find_element(By.TAG_NAME, 'img')
+    thumbnail_url = thumbnail_tag.get_attribute('src')
+    thumbnail_urls.append(thumbnail_url)
+
+    channel_tag = video.find_element(By.TAG_NAME, 'ytd-channel-name')
+    channel_name = channel_tag.text
+    channel_names.append(channel_name)
+
+    misc_tag = video.find_element(By.ID, 'metadata-line')
+    view = misc_tag.find_elements(By.TAG_NAME, 'span')[0].text
+    views.append(view)
+    upload = misc_tag.find_elements(By.TAG_NAME, 'span')[1].text
+    uploads.append(upload)
+
+    description = video.find_element(By.ID, 'description-text').text
+    descriptions.append(description)
+
+  videos_dict = {
+    'Title': titles,
+    'URL': urls,
+    'Views': views,
+    'Channel': channel_names,
+    'Uploaded': uploads,
+    'Description': descriptions,
+    'Thumbnail': thumbnail_urls
+  }
+
+  return videos_dict
+
+
+
+
 if __name__ == "__main__":
   print('Creating driver...')
   driver = get_driver()
@@ -34,35 +82,14 @@ if __name__ == "__main__":
 
   print(f"Found {len(videos)} videos")
 
-  print('Parsing the first video..')
-
   # title, url, thumbnail_url, channel, views, uploaded, description
+  print('Fetching videos data...')
+  videos_dict = get_details(videos)
+  
+  videos_df = pd.DataFrame(videos_dict)
 
-  video = videos[0]
-
-  title_tag = video.find_element(By.ID, 'video-title')
-  url = title_tag.get_attribute('href')
-  title = title_tag.text
-
-  thumbnail_tag = video.find_element(By.TAG_NAME, 'img')
-  thumbnail_url = thumbnail_tag.get_attribute('src')
-
-  channel_tag = video.find_element(By.TAG_NAME, 'ytd-channel-name')
-  channel_name = channel_tag.text
-
-  misc_tag = video.find_element(By.ID, 'metadata-line')
-  views = misc_tag.find_elements(By.TAG_NAME, 'span')[0].text
-  uploaded = misc_tag.find_elements(By.TAG_NAME, 'span')[1].text
-
-  description = video.find_element(By.ID, 'description-text').text
-  print(' ')
-  print("Title: ",title)
-  print("URL: ",url)
-  print('Thumbnail URL: ', thumbnail_url)
-  print('Channel name: ', channel_name)
-  print('Views: ', views)
-  print('Uploaded: ', uploaded)
-  print('Description: ', description)
+  print('saving to CSV')
+  videos_df.to_csv('videos.csv', index = False)
 
 
 
